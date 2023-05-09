3023924952e25c805c4fae920565fe5106ed0d95
unknown <donofrio.8@wright.edu>
2023-05-09 14:45:12 -0400

    first commit

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Static belongs to class
+            // not static belongs to object/instance
+
+            double celsius = 25;
+            double fahrenheit = 77;
+
+            Console.WriteLine($"{celsius} celsius is {TempConverter.CelsiusToFahrenheit(celsius)} in fahrenheit");
+
+            Console.WriteLine($"{fahrenheit} fahrenheit is {TempConverter.FahrenheitToCelsius(fahrenheit)} in celsius");
         }
     }
 }
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        //Static belongs to class
+        // not static belongs to object/instance
+
+        public static double FahrenheitToCelsius (double fahrenheit)
+        {
+
+            return ((fahrenheit - 32.0) * 5.0 / 9.0);
+        }
+
+        public static double CelsiusToFahrenheit(double celsius)
+        {
+
+            return ((celsius * (9.0/5.0) ) + 32.0);
+        }
+    }
+}
